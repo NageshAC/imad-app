5d492dcddfaf3ef989df248b020e897da6275ac6
acnagesh481 <acnagesh481@gmail.com>
2017-08-06 08:28:07 +0000

    Added article-one.html

--- server.js
+++ server.js
@@ -18,10 +18,16 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
+app.get('/ui/article-one.html'function(req,res){
+  res.sendfile(path.join(_dirname, 'ui', 'article-one.html'));
+})
+
+
+
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
+var port = 7070;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
