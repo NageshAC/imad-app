ec44d922c4cbb32a6dafa3a6f453095b164987b2
acnagesh481@gmail.com <acnagesh481@gmail.com>
2017-08-20 06:59:08 +0000

    added main.js to server.js

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
-app.get('/ui/article-one.html',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/ui/main.js',function(req,res){
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 
