a4a3da42cde4be147edf9a4acd2e54ef758e3870
Nagesh A C <30718386+NageshAC@users.noreply.github.com>
2017-08-26 14:01:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,17 @@
 var button = document.getElementById("counter");
 button.onClick = function(){
-    var reqt = new XMLHttpRequest();
-    reqt.onreadystatechange=function(){
-        if (reqt.readySate ===XMLHttpRequest.DONE){
-            if (reqt.status===200){
-                var counter = reqt.responseText;
+   // var reqt = new XMLHttpRequest();
+    //reqt.onreadystatechange=function(){
+      //  if (reqt.readySate ===XMLHttpRequest.DONE){
+        //    if (reqt.status===200){
+          //      var counter = reqt.responseText;
+                counter +=1;
                 var span = document.getElementById("count");
                 span.innerHTML = counter.toString();
-            }
-        }
-    };
-    reqt.open('GET', 'http://acnagesh481.imad.hasura-app.io/counter/',true)
-    reqt.send(null);
+            //}
+       // }
+   // };
+   // reqt.open('GET', 'http://acnagesh481.imad.hasura-app.io/counter/',true)
+   // reqt.send(null);
 
 };
