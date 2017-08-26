4fc33b4192de3bc369ece56d275ef0348854dc02
Nagesh A C <30718386+NageshAC@users.noreply.github.com>
2017-08-26 13:58:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
-var button = document.getElementById("co");
+var button = document.getElementById("counter");
 button.onClick = function(){
     var reqt = new XMLHttpRequest();
     reqt.onreadystatechange=function(){
         if (reqt.readySate ===XMLHttpRequest.DONE){
             if (reqt.status===200){
                 var counter = reqt.responseText;
-                var span = document.getElementById("c");
+                var span = document.getElementById("count");
                 span.innerHTML = counter.toString();
             }
         }
