f93f1cbf64de03143a27206025c52b508e0be88e
Nagesh A C <30718386+NageshAC@users.noreply.github.com>
2017-08-26 10:02:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/ui/main.js',function(req,res){
 });
 
 var counter=0;
-app.get('/counter/',function(req,req){
+app.get('/counter/',function(req,res){
    counter += 1;
    res.send(counter.toString());
 });
