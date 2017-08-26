22b311a02eea64366870186098ba9051e7607c15
Nagesh A C <30718386+NageshAC@users.noreply.github.com>
2017-08-26 20:57:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ var express = require('express');
  
  
  var port = 80; // Use 8080 for local development because you might already have apache running on 80
- app.listen(8080, function () {
+ app.listen(80, function () {
    console.log(`IMAD course app listening on port ${port}!`);
  });
