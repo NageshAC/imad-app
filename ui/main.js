c0c89aaa8e775daa218a18b868a52b7a27c0cc04
acnagesh481@gmail.com <acnagesh481@gmail.com>
2017-08-26 12:37:56 +0000

    alert

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,1 @@
-var button = document.getElementById("counter");
-var counter = 0;
-button.onClick = function(){
-   // var reqt = new XMLHttpRequest();
-    //reqt.onreadystatechange=function(){
-      //  if (reqt.readySate ===XMLHttpRequest.DONE){
-        //    if (reqt.status===200){
-          //      var counter = reqt.responseText;
-                counter +=1;
-                var span = document.getElementById("count");
-                span.innerHTML = counter.toString();
-            //}
-       // }
-   // };
-   // reqt.open('GET', 'http://acnagesh481.imad.hasura-app.io/counter/',true)
-   // reqt.send(null);
-
-};
+alert ("Hi,  Nagesh. The main.js is responding.");
