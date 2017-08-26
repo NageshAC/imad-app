a7aa1c77bc856673318da9dc7ffe606314194596
acnagesh481@gmail.com <acnagesh481@gmail.com>
2017-08-26 05:04:04 +0000

    count-span

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,7 @@
-console.log('Loaded!');
-prompt("your age?");
+var button = document.getElementById('counter');
+var counter = 0 ;
+button.onClick = function(){
+counter += 1;
+var span = document.getElementById('count');
+span.innerHTML = counter.toString();
+};
