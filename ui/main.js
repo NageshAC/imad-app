c5474dd49090ff87f6a5e876b5ec2fda2c734467
acnagesh481@gmail.com <acnagesh481@gmail.com>
2017-08-26 13:13:37 +0000

    added search box programming

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 alert ("Hi,  Nagesh. The main.js is responding.");
+
+var name = document.getElementById('name');
+var submit = document.getElementById('submit_btn');
+var nameValue = name.value;
+submit.onclick = function (){
+	var list ='';
+	var name = ['name1', 'name2', 'name3'];
+	for (var i=0; i<3; i++){
+		list += '<li>' + name[i] + '</li>
+	};document.getElementById('nameList').innetHTML = list;
+};
