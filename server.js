1ea841f37bd9cc2476d798c0d2c10c638598c118
Nagesh A C <30718386+NageshAC@users.noreply.github.com>
2017-08-28 23:01:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
  var morgan = require('morgan');
  var path = require('path');
  var app = express();
- var pool = require('pg').pool;
+ var Pool = require('pg').pool;
  var config = {
      host:'db.imad.hasura-app.io',
      user:'acnagesh481',
